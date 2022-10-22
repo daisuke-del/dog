659de784938a1a2af9e868373b9effd289119170
daisuke-del <daisukeshiroma28@gmail.com>
2022-10-22 12:56:53 +0900

    toggleプラグインの追加。 signupの性別切り替えで使用

--- src/frontend/nuxt.config.js
+++ src/frontend/nuxt.config.js
@@ -28,7 +28,12 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    {
+      src: '@/plugins/toggle-button',
+      mode: 'client'
+    }
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
