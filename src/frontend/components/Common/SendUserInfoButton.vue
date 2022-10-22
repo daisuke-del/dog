c865e55433a4bac50b9a9228e7af0ee6e0818d9f
daisuke-del <daisukeshiroma28@gmail.com>
2022-10-22 13:10:55 +0900

    送信ボタンコンポーネント

---
+++ src/frontend/components/Common/SendUserInfoButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="button">
+    <v-btn
+      class="submit-button"
+      depressed
+      block
+      color="primary"
+      height="55px"
+      :loading="sendLoading"
+      @click="clickSendUserInfo"
+    >
+      <v-icon left>
+        mdi-email-outline
+      </v-icon>
+      入力内容を送信
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    sendLoading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    clickSendUserInfo () {
+      this.$emit('send-user-info')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.submit-button {
+  font-weight: bolder;
+  font-size: 16px;
+}
+</style>
