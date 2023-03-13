7fee6731f3d985b9afeee4d9572ce54e46c4be7b
daisuke-del <daisukeshiroma28@gmail.com>
2023-03-13 21:16:46 +0900

    xdebug削除、アイコン変更

--- src/frontend/nuxt.config.js
+++ src/frontend/nuxt.config.js
@@ -4,13 +4,13 @@ const environment = process.env.NODE_ENV
 const envSet = require(`./env.${environment}.js`)
 
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'server',
+  env: envSet,
+  ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - frontend',
-    title: 'frontend',
+    titleTemplate: '%s - marigold',
+    title: 'marigold',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
