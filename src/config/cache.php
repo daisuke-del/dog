66dd3427e14e8ca71b721a821c4c80ca09d1c84b
daisuke-del <daisukeshiroma28@gmail.com>
2023-03-16 22:19:18 +0900

    エラーが出たためキャッシュドライバーを変更

--- src/config/cache.php
+++ src/config/cache.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_DRIVER', 'cookie'),
+    'default' => env('CACHE_DRIVER', 'file'),
 
     /*
     |--------------------------------------------------------------------------
