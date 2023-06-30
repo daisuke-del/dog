4cadd6bd0f4897099da4a3d2f5fc280b3b8170d6
daisuke-del <daisukeshiroma28@gmail.com>
2023-06-30 09:10:28 +0900

    ログイン状態の維持

--- src/frontend/store/index.js
+++ src/frontend/store/index.js
@@ -1,17 +1,17 @@
-// import user from '@/plugins/modules/user'
+import user from '@/plugins/modules/user'
 
-// const cookieparser = process.server ? require('cookieparser') : undefined
+const cookieparser = process.server ? require('cookieparser') : undefined
 
-// export const actions = {
-//   async nuxtServerInit({ commit }, { req }) {
-//     cookieparser.parse(req.headers.cookie)
+export const actions = {
+  async nuxtServerInit({ commit }, { req }) {
+    cookieparser.parse(req.headers.cookie)
 
-//     if (!req.headers.cookie.split(';').includes(' auth._token.login=false')) {
-//       await user.getUserInfo().then((response) => {
-//         commit('authInfo/setAuthInfo', response)
-//       }).catch(() => {
-//           commit('authInfo/setAuthInfo', null)
-//       })
-//     }
-//   }
-// }
+    if (!req.headers.cookie.split(';').includes(' auth._token.login=false')) {
+      await user.getUserInfo().then((response) => {
+        commit('authInfo/setAuthInfo', response)
+      }).catch(() => {
+          commit('authInfo/setAuthInfo', null)
+      })
+    }
+  }
+}
