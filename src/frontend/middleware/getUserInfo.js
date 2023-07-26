a86bbd41c5685a09a471d2149a26ab6c611fcb6a
daisuke-del <daisukeshiroma28@gmail.com>
2023-07-27 08:47:58 +0900

    不要ファイルを削除

--- src/frontend/middleware/getUserInfo.js
+++
@@ -1,9 +0,0 @@
-import user from "@/plugins/modules/user"
-
-export default function ({ app }) {
-    if (app.$auth.loggedIn) {
-        user.getUserInfo().then((response) => {
-          app.store.dispatch('authInfo/setAuthInfo', response)
-        })
-      }
-  }
