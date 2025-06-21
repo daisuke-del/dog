6f5206735b29b737c8485c0f614812ef0a381208
daisuke-del <daisukeshiroma28@gmail.com>
2025-06-21 13:05:34 +0900

    csrf_tokenを考慮

--- src/app/Http/Middleware/VerifyCsrfToken.php
+++ src/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'user/diagnosis/result'
+        'user/diagnosis/result',
+        'api/diagnosis/person',
     ];
 }
